e18ffc4223cdd1684720f3a923cd351513faa686
vero <veronica.delacruz12@hotmail.com>
2023-04-09 13:37:09 -0600

    login and register adding  delivery food app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,43 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomePageModule),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'detail/:id',
-    loadChildren: () =>
-      import('./screens/detail/detail.module').then((m) => m.DetailPageModule),
-  },
+  // {
+  //   path: 'home',
+  //   loadChildren: () =>
+  //     import('./home/home.module').then((m) => m.HomePageModule),
+  // },
+//   {
+//     path: '',
+//     redirectTo: 'login',
+//     pathMatch: 'full',
+//   },
+//   {
+//     path: 'detail/:id',
+//     loadChildren: () =>
+//       import('./screens/detail/detail.module').then((m) => m.DetailPageModule),
+//   },
+// ];
+{
+  path: '',
+  redirectTo: 'login',
+  pathMatch: 'full'
+},
+{
+  path: 'login',
+  loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
+
+},
+{
+  path: 'registro',
+  loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule),
+
+},
+{
+     path: 'home',
+     loadChildren: () =>
+     import('./home/home.module').then((m) => m.HomePageModule),
+   },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,10 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  providers: [
+   
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
